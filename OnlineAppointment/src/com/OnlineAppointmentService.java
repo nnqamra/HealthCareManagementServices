32f1a37b87f45ff227df1f4a3ae2f840ab88774c
nnqamra <noorrulqamra@gmail.com>
2020-04-18 20:34:31 +0530

    Implemented addAppointment method and get request

--- OnlineAppointment/src/com/OnlineAppointmentService.java
+++ OnlineAppointment/src/com/OnlineAppointmentService.java
@@ -1,5 +1,40 @@
 package com;
 
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+
+//For JSON
+import com.google.gson.*;
+
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+import model.OnlineAppointment;
+
+
+@Path("/Appointments")
+
 public class OnlineAppointmentService {
+	
+	OnlineAppointment App1 = new OnlineAppointment();
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String ReadAppointment() {
+
+		return "Hello";
+	}
+
 
 }
--- OnlineAppointment/src/model/OnlineAppointment.java
+++ OnlineAppointment/src/model/OnlineAppointment.java
@@ -1,5 +1,43 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import config.DBConnector;
+
+
 public class OnlineAppointment {
+	
+	public String addAppointment(Date date, String time, String desc, int pid, int did, int hid) {
+
+		try (Connection con = DBConnector.getConnection()) {
+
+			String addAppQuery = " insert into appointment (`apmnt_id`,`date`,`time`,`apmnt_desc`,`patient_id`,`doctor_id`,`hospital_id`)"
+					+ "values(NULL, ?, ?, ?, ?, ?, ?)";
+
+			PreparedStatement pstmnt = con.prepareStatement(addAppQuery);
+
+			pstmnt.setInt(1, 0);
+			pstmnt.setDate(1, date);
+			pstmnt.setString(2, time);
+			pstmnt.setString(3, desc);
+			pstmnt.setInt(4, pid);
+			pstmnt.setInt(5, did);
+			pstmnt.setInt(6, hid);
+
+			pstmnt.execute();
+			return "Appointment added successfully";
+
+		} catch (SQLException e) {
+			return "Error occured during adding an Appointment\n" + e.getMessage();
+		}
+
+	}
+
 
 }
