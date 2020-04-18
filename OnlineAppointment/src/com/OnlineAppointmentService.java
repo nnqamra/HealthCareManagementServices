eac8e6f9c5052884bef9d53bdfc483ac6caab8ab
nnqamra <noorrulqamra@gmail.com>
2020-04-18 21:22:35 +0530

    Implemented delete request

--- OnlineAppointment/src/com/OnlineAppointmentService.java
+++ OnlineAppointment/src/com/OnlineAppointmentService.java
@@ -31,40 +31,41 @@ public class OnlineAppointmentService {
 		return App1.ReadAppointment();
 
 	}
-	
 
 	@POST
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String addAppointment(
-			@FormParam("date") Date date,
-			@FormParam("time") String time,
-			@FormParam("apmnt_desc") String desc,
-			@FormParam("patient_id") int pid,
-			@FormParam("doctor_id") int did,
+	public String addAppointment(@FormParam("date") Date date, @FormParam("time") String time,
+			@FormParam("apmnt_desc") String desc, @FormParam("patient_id") int pid, @FormParam("doctor_id") int did,
 			@FormParam("hospital_id") int hid) {
 		String output = App1.addAppointment(date, time, desc, pid, did, hid);
 		return output;
 	}
-	
-	
+
 	@PUT
-	@Path("/App1/{apmnt_id}/") 
+	@Path("/App1/{apmnt_id}/")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_PLAIN)
 	public String UpdateAppointment(
-			
-			@FormParam("date") Date date, 
-			@FormParam("time") String time,
-			@FormParam("apmnt_desc") String desc,
-			@PathParam("apmnt_id") int AppID
-			) 
-	{
-		String output = App1.UpdateAppointment(date,time,desc,AppID);
+
+			@FormParam("date") Date date, @FormParam("time") String time, @FormParam("apmnt_desc") String desc,
+			@PathParam("apmnt_id") int AppID) {
+		String output = App1.UpdateAppointment(date, time, desc, AppID);
 		System.out.println(AppID);
 		return output;
 	}
 
+	@DELETE
+	@Path("/delete/App1/{apmnt_id}/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String DeleteAppointment(@PathParam("apmnt_id") int AppID) {
+
+		// Read the value from the element <AppID>
+		String output = App1.DeleteAppointment(AppID);
+		return output;
+
+	}
 
 }
